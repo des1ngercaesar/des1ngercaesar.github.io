c456df79be74e5018d813771fc68be207929795f
des1ngercaesar <tristannugraha789@gmail.com>
2026-07-16 15:01:18 +0700

    Update script.js

--- TikGrid/script.js
+++ TikGrid/script.js
@@ -1,1 +1,13 @@
+const buttons = document.querySelectorAll(".layout-btn");
 
+buttons.forEach(button=>{
+
+button.onclick=()=>{
+
+buttons.forEach(b=>b.classList.remove("active"));
+
+button.classList.add("active");
+
+}
+
+});
